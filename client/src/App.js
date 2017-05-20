b64c48eeda917ff13a9afa13e23e537c1141e53e
jgladch <jeffgladchun@gmail.com>
2017-05-20 14:03:38 -0400

    Fetching authorization code from Google, sending to server. Need to implement server-side auth code exchange.
    	https://developers.google.com/identity/sign-in/web/server-side-flow

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,11 @@
 import React, { Component } from 'react';
+import GoogleLogin from 'react-google-login';
+import axios from 'axios';
+
+const responseGoogle = (response) => {
+  console.log(response);
+  axios.post('/auth', response).then(response => console.log('response: ', response)).catch(err => console.log(err));
+}
 
 class App extends Component {
   constructor() {
@@ -11,7 +18,15 @@ class App extends Component {
   render() {
     return (
       <div className='App'>
-        Fartosaurus Rex
+        <GoogleLogin
+          clientId='19384924645-o5equrcis2e7oo46scvlgpn7fe8e6t4g.apps.googleusercontent.com'
+          buttonText='Login'
+          responseType={'id_token token'}
+          offline={true}
+          scope={'https://www.googleapis.com/auth/admin.directory.resource.calendar'}
+          onSuccess={responseGoogle}
+          onFailure={responseGoogle}
+        />
       </div>
     );
   }
--- server/lib/server.js
+++ server/lib/server.js
@@ -1,13 +1,45 @@
 import express from 'express';
+import passport from 'passport';
+import { Strategy } from 'passport-google-authcode';
+import bodyParser from 'body-parser';
+
 const app = express();
 
+// Environment variables in development
+if (process.env.NODE_ENV !== 'production') { // If we're not in production, pull in environment variables
+  require('dotenv').config();
+}
+
+// Middleware
 app.set('port', (process.env.PORT || 3001));
+app.use(bodyParser.json());
 
 if (process.env.NODE_ENV === 'production') { // Express only serves static assets in production
   app.use(express.static('client/build'));
-} else { // If we're not in production, pull in environment variables
-  require('dotenv').config();
 }
+
+// Auth
+app.use(passport.initialize());
+
+passport.use(new Strategy({
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET
+}, (accessToken, refreshToken, profile, done) => {
+  console.log('accessToken: ', accessToken);
+  console.log('refreshToken: ', refreshToken);
+  console.log('profile: ', profile);
+  return done();
+}));
+
+// Routes
+app.get('/data', (req, res) => {
+  return res.send(['1', '2', '3']);
+});
+
+app.post('/auth', passport.authenticate('google-authcode'), (req, res) => {
+  console.log('auth post request: ', req.body);
+  return res.end();
+});
 
 app.listen(app.get('port'), () => {
   console.log(`Find the server at: http://localhost:${app.get('port')}/`); // eslint-disable-line no-console
