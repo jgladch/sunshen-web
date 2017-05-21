8fed273346dd4dd55be80d2ab4d96b6a17ec011b
jgladch <jeffgladchun@gmail.com>
2017-05-20 22:32:39 -0400

    Add basic Google Calendar API Call

--- server/lib/server.js
+++ server/lib/server.js
@@ -4,14 +4,12 @@ import googleAuth from 'google-auth-library';
 import bodyParser from 'body-parser';
 
 const app = express();
+const calendar = google.calendar('v3');
 
 // Environment variables in development
 if (process.env.NODE_ENV !== 'production') { // If we're not in production, pull in environment variables
   require('dotenv').config();
 }
-
-const auth = new googleAuth();
-const oauth2Client = new auth.OAuth2(process.env.GOOGLE_CLIENT_ID, process.env.GOOGLE_CLIENT_SECRET, process.env.GOOGLE_CLIENT_REDIRECT_URI);
 
 // Middleware
 app.set('port', (process.env.PORT || 3001));
@@ -27,6 +25,9 @@ app.get('/data', (req, res) => {
 });
 
 app.post('/auth', (req, res) => {
+  const auth = new googleAuth();
+  const oauth2Client = new auth.OAuth2(process.env.GOOGLE_CLIENT_ID, process.env.GOOGLE_CLIENT_SECRET, process.env.GOOGLE_CLIENT_REDIRECT_URI);
+
   return oauth2Client.getToken(req.body.code, (err, token) => {
     if (err) {
       console.log('Error while trying to retrieve access token', err);
@@ -34,8 +35,39 @@ app.post('/auth', (req, res) => {
     } else {
       console.log('Token: ', token);
     }
+    
     oauth2Client.credentials = token;
-    return res.end();
+
+    calendar.events.list({
+      auth: oauth2Client,
+      calendarId: 'primary',
+      timeMin: (new Date()).toISOString(),
+      maxResults: 10,
+      singleEvents: true,
+      orderBy: 'startTime'
+    }, (err, response) => {
+      if (err) {
+        console.log('The API returned an error: ' + err);
+        return;
+      }
+
+      const events = response.items;
+
+      console.log('events');
+
+      if (events.length == 0) {
+        console.log('No upcoming events found.');
+      } else {
+        console.log('Upcoming 10 events:');
+        for (var i = 0; i < events.length; i++) {
+          var event = events[i];
+          var start = event.start.dateTime || event.start.date;
+          console.log('%s - %s', start, event.summary);
+        }
+      }
+
+      return res.json(events);
+    });
   });
 });
 
